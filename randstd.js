c31635b228e33585722115e2dae7fab915839a7d
BarthdryZ <94731877+BarthdryZ@users.noreply.github.com>
2022-08-29 22:55:15 +0530

    improved website

---
+++ randstd.js
@@ -0,0 +1,36 @@
+const student = [
+	"Abhinesh",
+	"Barath",
+	"You",
+	"Harshavardhini",
+	"Madhu",
+	"Pravit:O",
+	"Rahul",
+	"Sanjay",
+	"Sarvanan",
+	"Shreyeshkar",
+	"Srithan",
+	"Karthik",
+	"Sushmitha",
+	"Vignesh",
+	"Navin",
+	"Naveen",
+	"Kamesh:O",
+	"Vishnu",
+	"Netha",
+	"Dedeepya",
+	"Harini",
+	"You",
+	"Venkat",
+	"Jaypreeth",
+	"Purenjay",
+	"Drona",
+	"Mukesh"
+]
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+var num = getRndInteger(0,26)
+console.log(num)
+console.log(student[num])
+document.getElementById("randstd").innerHTML =  "hello"
